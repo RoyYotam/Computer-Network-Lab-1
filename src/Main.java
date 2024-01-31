730c2fcc709130302831370e79757e7f6c5a7b41
itay022000 <158269338+itay022000@users.noreply.github.com>
2024-01-31 14:19:47 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         try {
             ConfigReader configReader = new ConfigReader();
-            int port = configReader.getPort(); 
-            String rootDirectory = configReader.getRootDirectory();
-            String defaultPage = configReader.getDefaultPage();
-            int maxThreads = configReader.getMaxThreads();
+            int port = configReader.get(port); 
+            String rootDirectory = configReader.get(root);
+            String defaultPage = configReader.get(defaultPage);
+            int maxThreads = configReader.get(maxThreads);
 
             Server server = new Server(port, rootDirectory, defaultPage, maxThreads);
             server.initializeServer();
